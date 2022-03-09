47a825a62319b6f6814b816e94e7659830a7a49d
julienl <julien.lengrandlambert@adyen.com>
2022-03-09 23:06:49 +0100

    Initiates repo with simple script to generate user

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.5.31"
+}
+
+group = "me.jlengrand"
+version = "1.0-SNAPSHOT"
+val ktorVersion = "1.6.6"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testCompile("junit", "junit", "4.12")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "create-developer-account"
+
