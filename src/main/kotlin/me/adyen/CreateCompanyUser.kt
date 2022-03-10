7973ee2c46dfc8d586678f0f73d4a2c8f9226071
Julien Lengrand-Lambert <julien@lengrand.fr>
2022-03-10 14:23:54 +0100

    Cleans up proof of concept

---
+++ src/main/kotlin/me/adyen/CreateCompanyUser.kt
@@ -0,0 +1,42 @@
+package me.adyen
+
+suspend fun main(args: Array<String>){
+    createEngineeringTestUser("test@adyen.com", "test", "user")
+}
+
+// Just building what I need now, we can dive further into profiles later as we need to expand
+suspend fun createEngineeringTestUser(email: String, firstName: String, lastName: String){
+    val managementApi = ManagementApi()
+
+    val defaultEngineeringTestUserRoles = listOf(
+        "Merchant standard role",
+        "Merchant technical integrator",
+        "Merchant admin",
+        "Manage API credentials",
+        "Merchant POS standard role",
+//    "View payments"
+    )
+
+    val companyId = "AdyenRecruitment"
+    val merchantAccount = "AdyenRecruitmentCOM"
+
+    managementApi.createUser(companyId,
+        User(
+            createUsername(firstName, lastName),
+            email,
+            Name(firstName, lastName),
+            defaultEngineeringTestUserRoles,
+            listOf(merchantAccount)
+        )
+    )
+}
+
+// Sanitizes the input string to match the Management API recommendations
+private fun String.sanitize(): String {
+    val regex = Regex.fromLiteral("[a-zA-Z0-9_\\-\\.]+")
+    return regex.replace(this,  "")
+}
+
+fun createUsername(firstName: String, lastName: String): String {
+    return "${firstName.sanitize()}.${lastName.sanitize()}.recruitment"
+}
