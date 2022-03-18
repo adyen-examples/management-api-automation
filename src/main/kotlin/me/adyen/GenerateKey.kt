98b0b051e58daa0215bcb8fd02422d1b7e4047ed
Jilling Kingma <jilling.kingma@adyen.com>
2022-03-18 18:51:56 +0100

    cleanup 2.0

---
+++ src/main/kotlin/me/adyen/GenerateKey.kt
@@ -0,0 +1,20 @@
+package me.adyen
+
+suspend fun main(args: Array<String>){
+    // Give a company name, user and specify which key you want to (re-)generate
+    generateKey("TestCompany123", "ws_211701@Company.TestCompany123", Specification.Clientkey)
+}
+
+// Just building what I need now, we can dive further into profiles later as we need to expand
+suspend fun generateKey(company: String, user: String, spec: Specification) {
+    val managementApi = ManagementApi()
+    // getting the credentials of the company
+    val credentials = managementApi.getapiCredentials(company)
+    println(credentials)
+    // Generating new Clientkey for specified credential ID and company
+    val credential = credentials.data.find { it.username == user}
+    if (credential != null) {
+        managementApi.generateClientKey(company, credential.id, spec)
+    }
+}
+
