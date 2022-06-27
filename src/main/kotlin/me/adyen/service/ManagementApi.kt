c59d46495ac4e1266a8332f42e11840d96e3bf1f
Julien Lengrand-Lambert <julien@lengrand.fr>
2022-06-27 14:00:17 +0200

    Upgrade deps

--- src/main/kotlin/me/adyen/service/ManagementApi.kt
+++ src/main/kotlin/me/adyen/service/ManagementApi.kt
@@ -2,8 +2,8 @@ package me.adyen
 
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
+import io.ktor.client.plugins.json.*
+import io.ktor.client.plugins.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
@@ -15,38 +15,39 @@ enum class Environment {
     Live
 }
 
-class ManagementApi(env : Environment = Environment.Test) {
-    private val baseUrl = if(env == Environment.Test) "https://management-test.adyen.com/v1" else "https://management.adyen.com/v1"
+class ManagementApi(env: Environment = Environment.Test) {
+    private val baseUrl =
+        if (env == Environment.Test) "https://management-test.adyen.com/v1" else "https://management.adyen.com/v1"
     private val key: String? = System.getenv("MANAGEMENT_KEY")
 
-    init{
-        if (key == null){
+    init {
+        if (key == null) {
             println("No API Key value found for environment variable \$MANAGEMENT_KEY. Exiting...")
             exitProcess(0)
         }
     }
 
-    private val client = HttpClient(){ install(JsonFeature) {
-        serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
-            prettyPrint = true
-            isLenient = true
-            ignoreUnknownKeys = true
-        })
-    }
+    private val client = HttpClient() {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
     }
 
     @OptIn(InternalAPI::class)
-    suspend fun createUser(company : String, user : User){
+    suspend fun createUser(company: String, user: User) {
         val response: HttpResponse = client.post("$baseUrl/companies/$company/users") {
             contentType(ContentType.Application.Json)
             body = user
-            headers{
+            headers {
                 append("x-api-key", key!!)
             }
         }
         println(response.receive<String>())
     }
-
 
 
 }
